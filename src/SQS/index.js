8fd35e3e1c480d0ffe606b2153a1ace1106446bf
Simranjeet Singh <simranjeet2589@gmail.com>
2019-11-10 11:39:55 +0530

    [SQS Example for sending message to SQS]

---
+++ src/SQS/index.js
@@ -0,0 +1,29 @@
+/* Getting Started with AWS SQS using node js. This part shows how to send message to the SQS */
+
+
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const params = {
+  "MessageBody": JSON.stringify({
+    "foo": "bar",
+    "date": (new Date()).toISOString()
+  }),
+  "QueueUrl": "ADD YOUR QUEUE URL HERE"
+};
+
+// By using Callback
+sqs.sendMessage(params, (err, data) => {
+  if (err) {
+    console.log("There was an Error: ", err);
+  } else {
+    console.log("Successfully added message to queue", data.MessageId);
+  }
+});
+
+
+// Promise implementation
+sqs.sendMessage(params).promise()
+.then(data => console.log("Successfully added message to queue", data.MessageId))
+.catch(err => console.log("There was an Error: ", err));
