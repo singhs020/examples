e352183b4d039d5c82cb89b3a95fbda44f7f7b51
Simranjeet Singh <simranjeet2589@gmail.com>
2019-11-12 11:47:46 +0530

    [SQS Example for consuming message from SQS]

---
+++ src/SQS/consumingMessage.js
@@ -0,0 +1,46 @@
+/* Getting Started with AWS SQS using node js. This part shows how to consume message from the SQS */
+
+
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+const qurl = "ADD YOUR SQS URL HERE";
+
+const params = {
+  "QueueUrl": qurl,
+  "MaxNumberOfMessages": 1
+};
+
+sqs.receiveMessage(params, (err, data) => {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    if (!data.Message) { 
+      console.log('Nothing to process'); 
+      return;
+    }    
+    
+    const body = JSON.parse(data.Messages[0].Body);
+    console.log(body);
+
+    // process the body however you see fit.
+    // once the processing of the body is complete, delete the message from the SQS to avoid reprocessing it.
+
+    const delParams = {
+      "QueueUrl": qurl,
+      "ReceiptHandle": data.Messages[0].ReceiptHandle
+    };
+
+    sqs.deleteMessage(delParams, (err, data) => {
+      if (err) {
+        console.log("There was an error", err);
+      } else {
+        console.log("Message processed Successfully");
+      }
+    });
+  }
+});
+
+// the above message can be implemented using promise as well.
